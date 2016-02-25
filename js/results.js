7fa5048c120372b8c0f6240e2a6c28a737d907f3
Ian Badcoe <ian.badcoe@autodesk.com>
2016-02-25 16:26:19 +0000

    Clearer "loading" and "nothing found" indicators...

--- js/results.js
+++ js/results.js
@@ -8,14 +8,26 @@
 	      templateUrl: "templates/results_template.html",
 	      restrict: "E",
 	      controller: ['$scope', '$element', function ($scope, $element) {
+	         $scope.words = [];
+	         $scope.nothing = true;
+
 	         $scope.$watch("searchTerms", function (val) {
 	            $scope.words = [];
+
+	            $scope.nothing = false;
+
 	            if (!val || val === "") return;
+
+	            $scope.nothing = true;
 
 	            $scope.spinner = true;
 	            datamuseService.getdatamuse(val)
                  .success(function (response) {
-                    $scope.words = response;
+                    if (response && response.length > 0) {
+                       $scope.nothing = false;
+
+                       $scope.words = response;
+                    }
                  },
                  function (error) {
                     alert(error);
@@ -24,6 +36,18 @@
                     $scope.spinner = false;
                  });
 	         });
+
+	         $scope.getClass = function () {
+	            if ($scope.spinner) {
+	               return "results results-loading";
+	            }
+	            else if ($scope.nothing) {
+	               return "results results-nothing";
+	            }
+	            else {
+	               return "results";
+	            }
+	         };
 	      }]
 	   };
 	}]);
